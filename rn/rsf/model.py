aa4a85b8c3717e546c1c0323d575d8eed67a12d5
Rie Kamei <rie.kamei.r@gmail.com>
2022-01-06 00:57:09 -0600

    added multi-component snapshot class

--- rn/rsf/model.py
+++ rn/rsf/model.py
@@ -312,3 +312,101 @@ class TimeDomainModelSnap :
     self.x *= 1e-3
     self.z *= 1e-3
 
+class MCTimeDomainModelSnap :
+  def __init__fd(self, ref=None, 
+                nc=1,
+                oz=0., dz=1., nz=1,
+                ox=0., dx=1., nx=1, 
+                ot=0., dt=1., nt=1, init_value=0.):
+    if ref :
+      self.nx = ref.nx
+      self.ox = ref.ox
+      self.dx = ref.dx
+      self.nz = ref.nz
+      self.oz = ref.oz
+      self.dz = ref.dz
+      self.nt = ref.nt
+      self.dt = ref.dt
+      self.ot = ref.ot
+      self.nc = ref.nc
+
+    else :
+      self.nx = nx
+      self.ox = ox
+      self.dx = dx
+      self.nz = nz
+      self.oz = oz
+      self.dz = dz
+      self.nt = nt
+      self.dt = dt
+      self.ot = ot
+      self.nc = nc
+    self.initialize( init_value )
+
+  def initialize( self, init_value=0.0 ):
+    self.d = np.ones( ( self.nt, self.nc, self.nx, self.nz ), np.float32 
+                       ) * init_value
+
+  def initialise( self, init_value=0.0 ):
+    self.initialize( init_value  )
+
+
+  def read_header( self, input=None, f=None ) :
+    if input is None :
+      input = rsf.Input( f )
+    self.nt = input.int(   'n4' )
+    self.nz = input.int(   'n1' )
+    self.nx = input.int(   'n2' )
+    self.nc = input.int( 'n3' )
+    self.dt = input.float( 'd4' )
+    self.dz = input.float( 'd1' )
+    self.dx = input.float( 'd2' )
+    self.ot = input.float( 'o4' )
+    self.oz = input.float( 'o1' )
+    self.ox = input.float( 'o2' )
+ 
+
+  def read( self, input=None, f=None ):
+    flag = 0
+# def read( self, input=None, f=None, fbin=None ):
+    if input is None :
+      try :
+        input = rsf.Input( f )
+      except :
+        print( 'file %f cannot be found'%f )
+      flag = 1
+
+    self.read_header( input=input ) 
+    #print( self.dx )
+
+    self.initialize( 0.0 )
+    input.read(self.d)
+    self.set_axis()
+    if flag == 1 :
+      input.close()
+
+  def write( self, output=None, f=None ) :
+    if output is None :
+      output = rsf.Output( f )
+    print( 'this option is not implemented yet')
+    #output.put( 'n1', self.nz )
+    #output.put( 'n2', self.nx )
+    #output.put( 'd1', self.dz )
+    #output.put( 'd2', self.dx )
+    #output.put( 'o1', self.oz )
+    #output.put( 'o2', self.ox )
+    #output.write( self.d.astype( np.float32 ) )
+    #output.close()
+
+  def set_axis( self ) :
+    self.x = np.arange( 0, self.nx, dtype=np.float ) * self.dx + self.ox
+    self.z = np.arange( 0, self.nz, dtype=np.float ) * self.dz + self.oz
+    self.t = np.arange( 0, self.nt, dtype=np.float ) * self.dt + self.ot
+
+     
+
+  def m2km( self ) :
+    self.d *= 1e-3
+    self.x *= 1e-3
+    self.z *= 1e-3
+
